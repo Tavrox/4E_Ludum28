12bf895dd90886fc8b725aeaa6e5834b4d1702dd
Helari <babas-91@hotmail.fr>
2013-12-15 20:48:24 +0100

    lever

--- Assets/scripts/Gameplay/Lever.cs
+++ Assets/scripts/Gameplay/Lever.cs
@@ -7,22 +7,47 @@ public class Lever : MonoBehaviour {
 	public OTAnimatingSprite animSprite;
 	public List<TriggeredDoor> doors = new List<TriggeredDoor>();
 
+	public enum btnType { Lever, TimedBtn, SequenceBtn }
+	public btnType myButtonType;
+	public float delay = 0;
+	public bool trigged, seqLocked;
+
 	void OnTriggerStay(Collider other)
 	{
 		if (other.gameObject.CompareTag("Player"))
 		{
-			if (Input.GetKeyDown(KeyCode.F))
+			if (Input.GetKeyDown(KeyCode.F) && !seqLocked)
 			{
-				triggerLever();
+				trigged = !trigged;
+				if(trigged) animSprite.Play("unlock");
+				else animSprite.Play("lock");
+
+				if(myButtonType == btnType.SequenceBtn) {seqLocked = true;}
+				else {
+					triggerLever();
+					if(myButtonType == btnType.TimedBtn) {
+						StartCoroutine("delayRetrigg");
+					}
+				}
 			}
 		}
 		
 	}
-	void triggerLever()
+	IEnumerator delayRetrigg() {
+		yield return new WaitForSeconds(delay);
+		triggerLever();
+	}
+	public void triggerLever()
 	{
 		foreach (TriggeredDoor door in doors) {
-			if(door.isLocked) {animSprite.Play("unlock");door.Unlock();}
-			else {animSprite.Play("lock");door.Lock();}
+			if(door.isLocked) {door.Unlock();}
+			else {door.Lock();}
 		}
+	}
+	public IEnumerator resetLever () {
+		yield return new WaitForSeconds(1f);
+		seqLocked = false;
+		trigged = false;
+		animSprite.Play("lock");
 	}
 }
---
+++ Assets/scripts/Gameplay/SequenceBtn.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SequenceBtn : MonoBehaviour {
+	
+	public List<Lever> seqBtns = new List<Lever>();
+	private bool rightCombo = true, allTriggered = true, solved, rightComboChecked, errorDetected;
+	private int cptBtn;
+	// Use this for initialization
+	void Start () {
+		//StartCoroutine("myUpdate");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!errorDetected) checkRightCombo();
+		if(!solved) checkAllBtnTriggered();
+	}
+
+	private void checkRightCombo() {
+		cptBtn = 0;
+		rightCombo = true;
+		foreach (Lever btn in seqBtns) {
+			if(btn.trigged && cptBtn > 0) {
+				if(seqBtns[cptBtn-1].trigged == false) {
+					rightCombo = false;errorDetected = true;
+				}
+			}
+			cptBtn++;
+			if(cptBtn == seqBtns.Count) {
+				cptBtn --;
+			}
+		}
+	}
+	private void checkAllBtnTriggered() {
+		allTriggered = true;
+		foreach (Lever btn in seqBtns) {
+			if(btn.seqLocked == false) {
+				allTriggered = false;
+				}
+			}
+		if(allTriggered) {
+			if(errorDetected==false) {
+				foreach (Lever btn in seqBtns) {
+					btn.triggerLever();
+				}
+				solved = true;
+			}
+			else if(errorDetected == true) {
+
+				foreach (Lever btn in seqBtns) {
+					btn.StartCoroutine("resetLever");
+				}
+				StartCoroutine("resetRightCombo");
+			}
+		}
+	}
+	private IEnumerator resetRightCombo () {
+		yield return new WaitForSeconds(1.2f);
+		errorDetected = false;
+
+	}
+}
