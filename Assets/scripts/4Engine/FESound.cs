cd85231a478e52a13817456265d628fc2ae29afc
Tavrox <tavrox@gmail.com>
2014-01-19 19:26:35 +0100

    levels and tles

---
+++ Assets/scripts/4Engine/FESound.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FESound : MonoBehaviour {
+
+	public MasterAudioGroup SoundGroup;
+	[Range (0,10f)] public float Delay;
+	[Range (0,1f)] public float Volume;
+	[Range (0,1f)] public float Pitch;
+	public enum SoundType {Oneshot, Continuous};
+	public SoundType type;
+	public float RepeatRate = 0.6f;
+
+	public void playSound()
+	{
+		MasterAudio.PlaySound(SoundGroup.name, Volume, Pitch, Delay);
+	}
+	public void playModulatedSound(float _var1, float _var2)
+	{
+		float percent = (_var1 / _var2);	
+		Volume = percent;
+		MasterAudio.PlaySound(SoundGroup.name, Volume, Pitch, Delay);
+	}
+	public void playDistancedSound(Transform _obj1, Transform _obj2)
+	{
+		Vector2 pos1 = new Vector2(_obj1.position.x, _obj1.position.y);
+		Vector2 pos2 = new Vector2(_obj2.position.x, _obj2.position.y);
+		float res = Vector2.Distance(pos1, pos2);
+		Debug.Log ("Distance Sound" + res);
+//		MasterAudio.PlaySound(SoundGroup.name, Volume, Pitch, Delay);
+	}
+}
---
+++ Assets/scripts/Level/LevelBrick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBrick : MonoBehaviour {
+
+	public enum brickType
+	{
+		Lever,
+		Crate,
+		Walker,
+		Patroler,
+		TurretU,
+		Key,
+		TriggeredDoor,
+		TimedBtn,
+		SequencedBtn,
+		TeleportIn,
+		TeleportOut,
+		TrapKill,
+		BaseArcL,
+		ArcHorizontal,
+		ArcVertical,
+		BaseArcR,
+		BaseArcU,
+		BaseArcD,
+		EndDoor,
+		TurretD,
+		TurretL,
+		TurretR,
+		Start
+	};
+}
