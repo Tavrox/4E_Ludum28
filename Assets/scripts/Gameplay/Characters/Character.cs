3f47a6bb61a987c545259847965181f6befaada9
Helari <babas-91@hotmail.fr>
2014-03-10 18:34:15 +0100

    SautTest2

--- Assets/scripts/Gameplay/Characters/Character.cs
+++ Assets/scripts/Gameplay/Characters/Character.cs
@@ -152,7 +152,8 @@ public class Character : MonoBehaviour
 //					vectorMove.y = jump2Vel;
 //				}
 			}
-
+			jumpSpeed=12f;
+			if(jumpState>0.028f) jumpSpeed=jumpSpeed+1f;
 			vectorMove.y = jumpSpeed;
 			jumpState += 0.001f;
 			print (jumpState);
