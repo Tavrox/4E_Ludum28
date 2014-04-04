bafa5989d1faa18f9ee6157a5d7e14a4e7f26cda
Helari <babas-91@hotmail.fr>
2014-04-04 12:08:41 +0200

    BossRunner

---
+++ Assets/scripts/Gameplay/Characters/WalkerBossTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkerBossTrigger : MonoBehaviour {
+	
+	public WalkerBoss _walker;
+	private bool trigged;
+	private Player _player;
+	// Use this for initialization
+	void Start () {
+		//_walker = gameObject.GetComponent<Walker>();
+		GameEventManager.GameStart += GameStart;
+		_player = GameObject.Find("Player").GetComponent<Player>();
+		StartCoroutine("blockActivation");
+
+	}
+	IEnumerator blockActivation() {
+		yield return new WaitForSeconds(0.6f);
+		_walker.activated = false;
+	}
+	// Update is called once per frame
+	void OnTriggerEnter(Collider _other)
+	{
+		if (_other.CompareTag("Player") == true && _player.isDead == false)
+		{
+			_walker.activated = true;
+		}
+	}
+	
+	protected void GameStart () {
+		//if(FindObjectOfType(typeof(Enemy)) && this != null) {
+		if(this != null && gameObject.activeInHierarchy) {
+			StartCoroutine("blockActivation");
+		}
+	}
+}
