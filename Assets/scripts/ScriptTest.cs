0c933b779455eaaa9e3c59a45eedc3b95a1bc568
Helari <bastien.nanceau@gmail.com>
2014-06-19 17:48:52 +0200

    massattack

---
+++ Assets/scripts/ScriptTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("lpkihouhou");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
